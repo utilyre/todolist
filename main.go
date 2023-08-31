e78746528e5f3af827c0ede02d51ef42ac5b4fc6
Amirabbas Fazelinia <utilyre@proton.me>
2023-08-31 18:25:07 +0330

    ok route

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 
 	"github.com/joho/godotenv"
 	"github.com/utilyre/todolist/config"
@@ -18,7 +20,59 @@ func main() {
 		fx.Provide(
 			config.NewDatabaseConfig,
 			database.New,
+
+			asRoute(newOKHandler),
 		),
-		fx.Invoke(func(database.PostgresDatabase) {}),
+		fx.Invoke(
+			fx.Annotate(
+				newServeMux,
+				fx.ParamTags(`group:"routes"`),
+			),
+		),
 	).Run()
+}
+
+func asRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.ResultTags(`group:"routes"`),
+	)
+}
+
+type OKHandler struct{}
+
+func (h *OKHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (h *OKHandler) Pattern() string {
+	return "/ok"
+}
+
+func newOKHandler() Route {
+	return &OKHandler{}
+}
+
+type Route interface {
+	http.Handler
+
+	Pattern() string
+}
+
+func newServeMux(routes []Route, lc fx.Lifecycle) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	for _, route := range routes {
+		mux.Handle(route.Pattern(), route)
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go http.ListenAndServe(":5000", mux)
+			return nil
+		},
+	})
+
+	return mux
 }
